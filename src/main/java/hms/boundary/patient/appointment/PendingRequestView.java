a1ddd9b1c602612d927db75b190959c66a85b340
kekgan <keagan.c.p.kong@gmail.com>
2024-11-08 18:19:56 +0800

    fix: error when using .getAppointmentStatus on null appt

--- src/main/java/hms/boundary/patient/appointment/PendingRequestView.java
+++ src/main/java/hms/boundary/patient/appointment/PendingRequestView.java
@@ -12,14 +12,16 @@ import hms.repository.PatientRepository;
 public class PendingRequestView extends View{
     public Appointment displayFirstPendingAppointments(Schedule schedule, PatientRepository patientRepository){
         Map<LocalDate, Appointment[]> sm = schedule.getScheduleMap();
-        for (LocalDate key : sm.keySet()){
-            for (Appointment appt : sm.get(key)){
-                if (appt.getAppointmentStatus() == AppointmentStatus.PENDING){
-                    System.out.println(key +"\t" + appt.getTime() + ": " + 
-                    (patientRepository.getById(appt.getPatientId())).getName() //patient name
-                    + "(" + appt.getPatientId() + ")");
-                    System.out.print("Enter 1 to confirm and 0 to cancel: ");
-                    return appt;
+        for (Map.Entry<LocalDate, Appointment[]> entry : sm.entrySet()){
+            for (Appointment appt : entry.getValue()){
+                if (appt!=null){
+                    if (appt.getAppointmentStatus() == AppointmentStatus.PENDING){
+                        System.out.println(entry.getKey() +"\t" + appt.getTime() + ": " + 
+                        (patientRepository.getById(appt.getPatientId())).getName() //patient name
+                        + "(" + appt.getPatientId() + ")");
+                        System.out.print("Enter 1 to confirm and 0 to cancel: ");
+                        return appt;
+                    }
                 }
             }
         }
