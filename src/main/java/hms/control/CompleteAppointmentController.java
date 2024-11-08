4ce52237b0a31e7d8dfe68f4645a82b0c488c03d
kekgan <keagan.c.p.kong@gmail.com>
2024-11-08 19:04:59 +0800

    feat: add option to choose number of prescription given, change selection of Appointment

--- src/main/java/hms/control/CompleteAppointmentController.java
+++ src/main/java/hms/control/CompleteAppointmentController.java
@@ -1,12 +1,14 @@
 package hms.control;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 import hms.boundary.InputHandler;
 import hms.boundary.Prompt;
 import hms.boundary.patient.ScheduleView;
 import hms.boundary.patient.appointment.CompleteAppointmentView;
 import hms.entity.appointment.Appointment;
+import hms.entity.appointment.AppointmentStatus;
 import hms.entity.appointment.Schedule;
 import hms.entity.medicine.Medicine;
 import hms.entity.record.AppointmentOutcomeRecord;
@@ -15,18 +17,17 @@ import hms.entity.user.Patient;
 import hms.exceptions.InvalidChoiceFormatException;
 import hms.exceptions.InvalidChoiceValueException;
 import hms.exceptions.InvalidDateException;
+import hms.exceptions.InvalidTimeException;
 
 public class CompleteAppointmentController extends Controller{
     private ScheduleView scheduleView;
     private CompleteAppointmentView completeAppointmentView;
     private Doctor doctor;
-    private Patient patient;
 
-    public CompleteAppointmentController(Doctor doctor, Patient patient) {
+    public CompleteAppointmentController(Doctor doctor) {
         this.scheduleView = new ScheduleView();
         this.completeAppointmentView = new CompleteAppointmentView();
         this.doctor = doctor;
-        this.patient = patient;
     }
 
     public void navigate() {
@@ -40,36 +41,61 @@ public class CompleteAppointmentController extends Controller{
 
         Schedule schedule = doctor.getSchedule();
         Appointment[] appointmentArr = (schedule.getScheduleMap()).get(date);
-        scheduleView.displayAppointmentByDate(schedule, patientRepository, date);
-        
-        completeAppointmentView.displayChoice(appointmentArr.length);
-        int choice;
-        try{
-            choice = InputHandler.getChoice(1, 9);
-        } catch (InvalidChoiceFormatException | InvalidChoiceValueException e) {
+        int count=0;
+        for (Appointment appt : appointmentArr) {
+            if (appt==null) {
+                continue;
+            }
+            if (appt.getAppointmentStatus() == AppointmentStatus.CONFIRMED) {
+                count++;
+            }
+        }
+        if (count==0) {
+            completeAppointmentView.displayNoAppointments(date);
             return;
         }
+        scheduleView.displayAppointmentByDate(schedule, patientRepository, date);
+        
+        completeAppointmentView.displayAppointmentChoice();
+        LocalTime startTime;
+        try{
+            startTime = InputHandler.getTime();
+        } catch (InvalidTimeException e) {
+            return;
+        }
+        Appointment appointment = appointmentArr[Schedule.getTimeslot(startTime)];
+        Patient patient = patientRepository.getById(appointment.getPatientId());
 
         completeAppointmentView.displayServiceTypePrompt();
         String service = InputHandler.getString();
         completeAppointmentView.displayNotesPrompt();
         String notes = InputHandler.getString();
 
-        Appointment appointment = appointmentArr[choice];
         AppointmentOutcomeRecord appointmentRecord = doctor.completeAppointment(patient, appointment, service, notes);
 
-
-        completeAppointmentView.displayAddPrescriptionNamePrompt();
-        String medName = InputHandler.getString();
-        completeAppointmentView.displayAddPrescriptionQtyPrompt();
-        int medQty = -1;
-        while (medQty == -1){
+        completeAppointmentView.displayPrescriptionChoicePrompt();
+        int numPre = -1;
+        while (numPre == -1){
             try{
-                medQty = InputHandler.getChoice(0, 100);
+                numPre = InputHandler.getChoice(0, 10);
             } catch (InvalidChoiceFormatException | InvalidChoiceValueException e) {
                 continue;
             }
         }
-        appointmentRecord.addPrescribedMedicine(new Medicine(medName, medQty));
+
+        for (int i = 0; i < numPre; i++){
+            completeAppointmentView.displayAddPrescriptionNamePrompt();
+            String medName = InputHandler.getString();
+            completeAppointmentView.displayAddPrescriptionQtyPrompt();
+            int medQty = -1;
+            while (medQty == -1){
+                try{
+                    medQty = InputHandler.getChoice(0, 100);
+                } catch (InvalidChoiceFormatException | InvalidChoiceValueException e) {
+                    continue;
+                }
+            }
+            appointmentRecord.addPrescribedMedicine(new Medicine(medName, medQty));
+        }
     }
 }
